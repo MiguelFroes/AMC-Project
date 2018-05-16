44ab8a032f8628a9a2190f7c2096b355f06159e0
Unknown <margaridapinhoo@hotmail.com>
2018-05-16 19:01:03 +0100

    Atualização do Classificador
    Dá erros

--- src/Classificador.java
+++ src/Classificador.java
@@ -13,13 +13,24 @@ import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.LinkedList;
 import java.awt.event.ActionEvent;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 public class Classificador {
 
 	private JFrame frame;
-	private JTextField textField;
-	private String database;
+	private JTextField textParameters;
+	private String redebayes;
+	private int[] vector;
+	private BN bn;
+	private LinkedList<Double> res;
+	private JTextField textRes;
+	
 
 	/**
 	 * Launch the application.
@@ -35,7 +46,7 @@ public class Classificador {
 				}
 			}
 		});
-	}
+		}
 
 	/**
 	 * Create the application.
@@ -50,43 +61,28 @@ public class Classificador {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(240, 255, 255));
-		frame.setBounds(600, 400, 1128, 785);
+		frame.setBounds(600, 400, 1009, 699);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblClassificadorTanE = new JLabel("CLASSIFICATION APPLICATION");
 		lblClassificadorTanE.setHorizontalAlignment(SwingConstants.CENTER);
 		lblClassificadorTanE.setForeground(new Color(0, 128, 128));
-		lblClassificadorTanE.setFont(new Font("Dialog", Font.BOLD, 24));
-		lblClassificadorTanE.setBounds(309, 22, 483, 123);
+		lblClassificadorTanE.setFont(new Font("Dialog", Font.BOLD, 26));
+		lblClassificadorTanE.setBounds(302, 6, 483, 91);
 		frame.getContentPane().add(lblClassificadorTanE);
 		
-		JLabel lblClickBelowTo = new JLabel("Insert below your pacient's data:");
+		JLabel lblClickBelowTo = new JLabel("Insert your pacient's data below:");
 		lblClickBelowTo.setFont(new Font("Dialog", Font.PLAIN, 23));
-		lblClickBelowTo.setBounds(21, 307, 333, 53);
+		lblClickBelowTo.setBounds(21, 254, 387, 37);
 		frame.getContentPane().add(lblClickBelowTo);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 0), null, null, null));
-		textArea.setSelectionColor(new Color(255, 255, 255));
-		textArea.setForeground(new Color(0, 0, 0));
-		textArea.setFont(new Font("Dialog", Font.PLAIN, 23));
-		textArea.setBackground(new Color(255, 255, 255));
-		textArea.setBounds(21, 358, 966, 166);
-		frame.getContentPane().add(textArea);
+		JTextField textParameters = new JTextField();
+		textParameters.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textParameters.setBounds(237, 103, 445, 123);
+		frame.getContentPane().add(textParameters);
+		textParameters.setColumns(10);
 		
-		JButton btnclassify = new JButton("CLASSIFY");
-		btnclassify.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnclassify.setForeground(new Color(0, 128, 128));
-		btnclassify.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 13));
-		btnclassify.setBackground(new Color(255, 255, 255));
-		btnclassify.setBorder(new LineBorder(new Color(95, 158, 160), 2, true));
-		btnclassify.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnclassify.setBounds(316, 547, 470, 105);
-		frame.getContentPane().add(btnclassify);
 		
 		JButton btnload = new JButton("LOAD");
 		btnload.setForeground(new Color(0, 128, 128));
@@ -106,18 +102,110 @@ public class Classificador {
 					System.out.println("Cancelled");
 				else 
 					System.out.println("File selected: " + filename);
-				database = fd.getDirectory()+filename;
-				textField.setText(filename);
+				redebayes= fd.getDirectory()+filename;
+				textParameters.setText(filename);
 				
 			}
 		});
-		btnload.setBounds(21, 155, 184, 123);
+		btnload.setBounds(21, 104, 184, 123);
 		frame.getContentPane().add(btnload);		
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		textField.setBounds(238, 154, 333, 123);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 0), null, null, null));
+		textArea.setSelectionColor(new Color(255, 255, 255));
+		textArea.setForeground(new Color(0, 0, 0));
+		textArea.setFont(new Font("Dialog", Font.PLAIN, 23));
+		textArea.setBackground(new Color(255, 255, 255));
+		textArea.setBounds(21, 319, 966, 166);
+		frame.getContentPane().add(textArea);
+		
+		JTextField textRes = new JTextField();
+		textRes.setBounds(237, 501, 750, 131);
+		frame.getContentPane().add(textRes);
+		textRes.setColumns(10);
+		
+		JButton btnclassify = new JButton("CLASSIFY");
+		btnclassify.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				try {
+					String[] Input=textArea.getText().split(",");
+					vector=new int[Input.length];
+					for(int i=0;i<Input.length;i++) {
+						int x=Integer.parseInt(Input[i]);
+						if(x>vector[i]) {
+							vector[i]=x;
+						}
+					}
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}	
+				
+				try {
+				FileInputStream fis = new FileInputStream(textParameters.getText()); 
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				bn = (BN) ois.readObject();
+				
+				fis.close();
+				ois.close();
+				
+				} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+				// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				res=bn.prob(vector);
+				String text = new String ("The most likely class is " + res.get(1).toString() + "with the probability of " + res.get(2).toString());
+				System.out.println(text);
+				textRes.setText(text);
+			
+			}
+		});
+		btnclassify.setForeground(new Color(0, 128, 128));
+		btnclassify.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 25));
+		btnclassify.setBackground(new Color(255, 255, 255));
+		btnclassify.setBorder(new LineBorder(new Color(95, 158, 160), 2, true));
+		btnclassify.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnclassify.setBounds(21, 501, 184, 131);
+		frame.getContentPane().add(btnclassify);
+		
+		JLabel lblNewJgoodiesLabel = DefaultComponentFactory.getInstance().createLabel("Please insert the data as the following example: 0,1,1,1,1,1,1 (according to the number of parameters of the disease)");
+		lblNewJgoodiesLabel.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		lblNewJgoodiesLabel.setBounds(21, 291, 771, 16);
+		frame.getContentPane().add(lblNewJgoodiesLabel);
+		
+		JLabel lblNumberOfParameters = DefaultComponentFactory.getInstance().createLabel("Number of parameters:");
+		lblNumberOfParameters.setFont(new Font("Apple SD Gothic Neo", Font.BOLD, 20));
+		lblNumberOfParameters.setBounds(708, 109, 261, 23);
+		frame.getContentPane().add(lblNumberOfParameters);
+		
+		JLabel lblCancer = DefaultComponentFactory.getInstance().createLabel("Cancer: 10");
+		lblCancer.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 15));
+		lblCancer.setBounds(708, 150, 120, 16);
+		frame.getContentPane().add(lblCancer);
+		
+		JLabel lblDiabetes = DefaultComponentFactory.getInstance().createLabel("Diabetes: 8");
+		lblDiabetes.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 15));
+		lblDiabetes.setBounds(708, 192, 120, 16);
+		frame.getContentPane().add(lblDiabetes);
+		
+		JLabel lblHepatitis = DefaultComponentFactory.getInstance().createLabel("Hepatitis: 19");
+		lblHepatitis.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 15));
+		lblHepatitis.setBounds(813, 150, 120, 16);
+		frame.getContentPane().add(lblHepatitis);
+		
+		JLabel lblThyroid = DefaultComponentFactory.getInstance().createLabel("Thyroid: 20");
+		lblThyroid.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 15));
+		lblThyroid.setBounds(813, 192, 120, 16);
+		frame.getContentPane().add(lblThyroid);
+		
+		
 	}
 }
